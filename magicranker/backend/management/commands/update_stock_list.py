c2c3bb5ce30c85bb1577cb66dc9b309a592ccef6
Lex Toumbourou <lextoumbourou@gmail.com>
2013-06-17 22:02:17 +1000

    New fields in stock list script

--- magicranker/backend/management/commands/update_stock_list.py
+++ magicranker/backend/management/commands/update_stock_list.py
@@ -17,36 +17,32 @@ class Command(BaseCommand):
         """
         # Get the list of stocks from the ASX as a list
         stocks = asx.get_full_stock_list()
+
         # Get today's date
         today = datetime.today()
+
+        new_count = 0;
+        updated_count = 0;
+
         # Add stock_list to the db if they aren't there already
         if stocks:
             for stock in stocks:
                 stock, created = Detail.objects.get_or_create(
                     code=stock, name='', desc='',
-                    last_updated=today, is_listed=True)
-                if not created:
-                    stock.last_updated = today
-                    stock.is_listed = True
-                    stock.save()
+                    last_listed=today)
+                if created:
+                    new_count += 1;
+                    stock.first_listed = today
+                else:
+                    update_count += 1;
+                    stock.last_listed = today
+
+                stock.save()
                 self.stdout.write('Adding or updating {0}\n'.format(stock))
             return True
         else:
             logging.error('Failed to download stock list')
             return False
-
-    def _set_unlisted_companies(self):
-        """
-        Set companies that haven't been updated in 1 month, to unlisted
-        """
-        two_weeks_ago = (datetime.today() - timedelta(weeks=2)).date()
-        stocks = Detail.objects.filter(is_listed=True)
-        for stock in stocks:
-            if stock.last_updated <= two_weeks_ago:
-                self.stdout.write(
-                    '{0} has been marked unlisted\n'.format(stock))
-                stock.is_listed = False
-                stock.save()
 
     def handle(self, *args, **kwargs):
         if self._get_full_stock_list():
