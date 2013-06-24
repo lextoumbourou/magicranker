dd3d66b1752442f9b9985bf026d61f71f925be51
Lex Toumbourou <lextoumbourou@gmail.com>
2013-06-24 22:43:33 +1000

    Added email to update profile details script

--- magicranker/backend/management/commands/update_profile_details.py
+++ magicranker/backend/management/commands/update_profile_details.py
@@ -11,9 +11,30 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         stocks = Detail.objects.filter(is_listed=True)
+        update_count = 0
         for stock in stocks:
+            self.stdout.write('Updating {0}\n'.format(stock.code))
             yf = YahooFinance.YahooFinance(stock.code)
             code, name, description = yf.get_profile()
+
+            code = code.encode('ascii', 'ignore')
+            name = name.encode('ascii', 'ignore')
+            description = description.encode('ascii', 'ignore')
+
+            self.stdout.write(
+                'Got the following details: {0}\n {1}\n {2}\n'.format(
+                    code, name, description))
+
             stock.name = name
             stock.desc = description
             stock.save()
+            update_count += 1
+
+        title = 'Report: update profile details complete ({0})'.format(
+                datetime.now())
+
+        message = '{0} companies updated'.format(update_count)
+
+        send_mail(
+            title, message, 'reports@magicranker.com',
+            ['lextoumbourou@gmail.com'], fail_silently = False)
