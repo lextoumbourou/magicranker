11a87c324fa34d929f652483519a61639d19a8d6
Lex Toumbourou <lextoumbourou@gmail.com>
2013-06-26 21:45:37 +1000

    Replace function-based views with class-based

--- magicranker/rank/urls.py
+++ magicranker/rank/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
-from django.views.generic.simple import direct_to_template
 admin.autodiscover()
 
 urlpatterns = patterns(
