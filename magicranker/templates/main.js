24d81d757c0d6fa49b5bd8a6dfd9003d5437fce8
Lex Toumbourou <lextoumbourou@gmail.com>
2013-07-10 21:53:33 +1000

    Presentation fixes

--- magicranker/templates/main.js
+++ magicranker/templates/main.js
@@ -1,3 +1,27 @@
-$("#roe_check_btn").on('click', '', function() {
-    $("input[name='rank_roe']").val("1");
-});
+function click_option(field_name)
+{
+    $("#" + field_name + "_set_btn").on('click', '', function() {
+        if ($(this).hasClass('btn-primary'))
+        {
+            $(this).removeClass('btn-primary');
+        }
+        else
+        {
+            $(this).addClass('btn-primary');
+        }
+        var rank_input = $("input[name='" + field_name + "']");
+        if (rank_input.val() == "1")
+        {
+            rank_input.val("0");
+        }
+        else
+        {
+            rank_input.val("1");
+        }
+    });
+}
+
+click_option('rank_roe');
+click_option('rank_pe');
+click_option('filt_market_cap');
+click_option('filt_debt');
