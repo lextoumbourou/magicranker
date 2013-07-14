b4a253fff7ac4a4b3dfa3403e226b7f9886c402a
Lex Toumbourou <lextoumbourou@gmail.com>
2013-07-14 11:20:15 +1000

    Oh yeah, it's not PHP...

--- magicranker/backend/scrapers/ETrade.py
+++ magicranker/backend/scrapers/ETrade.py
@@ -132,7 +132,7 @@ class ETrade():
 
             data['periods'] = self._get_dates(trs)
             data['roe'] = self._get_values(trs, 'Return on equity (%)')
-            // Convert percentage to fraction
+            # Convert percentage to fraction
             data['roe'] = float(data['roe']) / 100.
 
             # Get 3rd last table (should have Per Share stats in it)
