80847db5203f29b95a4c2580104f7f36e4df5366
Lex Toumbourou <lextoumbourou@gmail.com>
2013-11-09 17:27:55 +1100

    Tests use dictionary

--- magicranker/rank/tests.py
+++ magicranker/rank/tests.py
@@ -10,31 +10,71 @@ class RankerTest(TestCase):
     fixtures = ['rank_stock_data.yaml']
 
     def testRankByPEReturnsLowestFirst(self):
-        pe_rank = RankMethod(name='pe', min=5)
+        pe_rank = {
+            'name': 'pe',
+            'min': {
+                'changeable': True,
+                'value': 5
+            },
+            'ascending': True
+        }
         ranker = Ranker([pe_rank], [], limit=50)
         results = ranker.process()
 
         self.assertTrue(results.code__code[0:1].values[0] == 'ARB')
 
     def testRankByROEReturnHighestFirst(self):
-        roe_rank = RankMethod(name='roe', max=0.70, ascending=False)
+        roe_rank = {
+            'name': 'roe',
+            'max': {
+                'changeable': True,
+                'value': 0.70
+            },
+            'ascending': False
+        }
         ranker = Ranker([roe_rank], [], limit=50)
         results = ranker.process()
 
         self.assertTrue(results.code__code[0:1].values[0] == 'SRX')
 
     def testRankAndFilterRemovesTooSmallCompanies(self):
-        market_cap_filter = FilterMethod(name='market_cap', min=5000000)
-        roe_rank = RankMethod(name='roe', max=0.70, ascending=False)
-
+        market_cap_filter = {
+            'name': 'market_cap',
+            'min': {
+                'changeable': True,
+                'value': 5000000
+            },
+        }
+        roe_rank = {
+            'name': 'roe',
+            'max': {
+                'changeable': True,
+                'value': 0.70
+            },
+            'ascending': False
+        }
         ranker = Ranker([roe_rank], [market_cap_filter], limit=50)
         results = ranker.process()
 
         self.assertTrue(results.code__code[0:1].values[0] == 'CBA')
 
     def testRankROEAndPEReturnsCorrectTop(self):
-        roe_rank = RankMethod(name='roe', max=0.70, ascending=False)
-        pe_rank = RankMethod(name='pe', min=5)
+        roe_rank = {
+            'name': 'roe',
+            'max': {
+                'changeable': True,
+                'value': 0.70
+            },
+            'ascending': False
+        }
+        pe_rank = {
+            'name': 'pe',
+            'min': {
+                'changeable': True,
+                'value': 5
+            },
+            'ascending': True
+        }
 
         ranker = Ranker([pe_rank, roe_rank], [], limit=50)
         results = ranker.process()
@@ -42,22 +82,21 @@ class RankerTest(TestCase):
         self.assertTrue(results.code__code[0:1].values[0] == 'SRX')
 
     def testRankROEAvg5ReturnsCorrectTop(self):
-        roe_rank = RankMethod(name='roe', max=0.80, average=5, ascending=False)
+        roe_rank = {
+            'name': 'roe',
+            'max': {
+                'changeable': True,
+                'value': 0.80,
+            },
+            'average': {
+                'changeable': True,
+                'value': 5
+            },
+            'ascending': False
+        }
 
         ranker = Ranker([roe_rank], [], limit=50)
         results = ranker.process()
 
         self.assertTrue(results.code__code[0:1].values[0] == 'ARB')
         self.assertTrue(results.code__code[-1:].values[0] == 'ANZ')
-
-    # This feature hasn't been written yet
-    #def testRankAndFilterRemovesCompaniesWithHighDebt(self):
-    #    debt_filter = FilterMethod(name='debt_percentage', max=0.50)
-    #    roe_rank = RankMethod(name='roe', max=10, ascending=False)
-    #    ranker = Ranker([roe_rank], [debt_filter], limit=50)
-    #    results = ranker.process()
-
-    #    import pdb; pdb.set_trace()
-    #    self.assertTrue(all([result['code'] != 'SRX' for result in results]))
-    #    self.assertTrue(any([result['code'] == 'ANZ' for result in results]))
-    #    self.assertTrue(len(results) == 2)
