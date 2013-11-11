5ab5b5ca0b0af0a325a296a1cdf2a7ad2c4d15c2
Lex Toumbourou <lextoumbourou@gmail.com>
2013-11-11 23:38:30 +1100

    Added new price history fields

--- magicranker/stock/models.py
+++ magicranker/stock/models.py
@@ -104,7 +104,11 @@ class BalSheet(models.Model):
 class PriceHistory(models.Model):
     code = models.ForeignKey(Detail)
     date = models.DateField()
-    close = models.DecimalField(max_digits=5, decimal_places=2)
+    open = models.DecimalField(max_digits=10, decimal_places=2)
+    high = models.DecimalField(max_digits=10, decimal_places=2)
+    low = models.DecimalField(max_digits=10, decimal_places=2)
+    close = models.DecimalField(max_digits=10, decimal_places=2)
+    adjusted_close = models.DecimalField(max_digits=10, decimal_places=2)
     volume = models.BigIntegerField()
 
     objects = DataFrameManager()
