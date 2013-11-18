394c711133e7127a960855dd640e5446f55243dc
Lex Toumbourou <lextoumbourou@gmail.com>
2013-11-18 22:14:09 +1100

    Added simulate_rank api url

--- magicranker/api/urls.py
+++ magicranker/api/urls.py
@@ -4,5 +4,6 @@ from magicranker.api import views
 
 urlpatterns = patterns('',
     url(r'^rank/', 'magicranker.api.views.rank'),
+    url(r'^simulate_rank/', 'magicranker.api.views.simulate_rank'),
     url(r'^get_all_controls/', 'magicranker.api.views.get_all_controls'),
 )
