473af285dc7c4ec89f0dff0c9b616f9aecb3155c
Lex Toumbourou <lextoumbourou@gmail.com>
2017-01-21 13:32:48 +1100

    Simplified and finished fixing Yahoo Finance scraper.

--- magicranker/backend/management/commands/update_key_stats.py
+++ magicranker/backend/management/commands/update_key_stats.py
@@ -1,8 +1,10 @@
+import pprint
+
 from datetime import datetime
 
 from django.core.management.base import BaseCommand
 
-from magicranker.backend.scrapers import YahooFinance
+from magicranker.backend.scrapers import yahoo_finance
 from magicranker.stock.models import Detail, PriceHistory, PerShare, BalSheet
 
 
@@ -18,16 +20,17 @@ def get_total_debt_ratio(stock, date):
 
 
 class Command(BaseCommand):
+
     help = 'Get Profile Details from YahooFinance'
 
-    def _update_latest_price(self, stock, date, yf):
-        # get today's price information
-        price_data = yf.get_current_price()
+    def _update_latest_price(self, stock, date):
+        """Get latest price and update DB."""
+        price_data = yahoo_finance.get_current_price(stock)
+
         if price_data:
-            code, date, price, volume = price_data
             self.stdout.write(
-                'Attempting to update {0} with {1}, {2}, {3}\n'.format(
-                    stock.code, str(date), price, volume))
+                'Attempting to update {0}.\n'.format(
+                    pprint.pprint(price_data)))
             try:
                 price_history = (
                     PriceHistory.objects.get(code=stock, date=date))
@@ -35,27 +38,23 @@ class Command(BaseCommand):
                 price_history = None
 
             if price_history:
-                price_history.close = price
-                price_history.volume = volume
+                price_history.close = price_data.close
+                price_history.volume = price_data.volume
             else:
                 price_history = PriceHistory(
-                    code=stock, date=date, close=price, volume=volume)
+                    code=stock, date=date, close=price_data.close,
+                    volume=price_data.volume)
             price_history.save()
 
             return True
         else:
             return False
 
-    def _update_key_stats(self, stock, date, yf):
+    def _update_key_stats(self, stock, date):
         """Get key statistics (relies on date collected above)."""
-        stats_data = yf.get_key_stats()
-        if stats_data:
-            code, eps, roe, bv, pe, mc = stats_data
-            self.stdout.write((
-                'Attempting to update {0} with '
-                '{1}, {2}, {3}, {4}, {5}\n').format(
-                    code, eps, roe, bv, pe, mc))
+        stats_data = yahoo_finance.get_key_stats(stock)
 
+        if stats_data:
             try:
                 per_share = PerShare.objects.get(
                     code=stock, date=date, year=date.year)
@@ -66,18 +65,17 @@ class Command(BaseCommand):
                 per_share = PerShare(
                     code=stock, date=date)
 
-            per_share.earnings = eps
-            per_share.roe = roe
-            per_share.book_value = bv
-            per_share.pe = pe
-            per_share.market_cap = mc
+            per_share.earnings = stats_data.eps
+            per_share.roe = stats_data.roe
+            per_share.roa = stats_data.roa
+            per_share.book_value = stats_data.bvps
+            per_share.pe = stats_data.pe
+            per_share.market_cap = stats_data.market_cap
             per_share.year = date.year
             per_share.total_debt_ratio = get_total_debt_ratio(stock, date)
             per_share.save()
 
-            self.stdout.write(
-                'Updating {0} with {1}, {2}, {3}, {4}, {5}\n'.format(
-                    stock.code, eps, roe, bv, pe, mc))
+            self.stdout.write('Update: {0}\n'.format(stats_data))
 
     def handle(self, *args, **kwargs):
         stocks = Detail.objects.filter(is_listed=True)
@@ -85,8 +83,7 @@ class Command(BaseCommand):
         for stock in stocks:
             self.stdout.write(
                 'Collecting data for {0}'.format(stock))
-            yf = YahooFinance.YahooFinance(stock.code)
             date = datetime.today().date()
-            if self._update_latest_price(stock, date, yf):
-                self._update_key_stats(stock, date, yf)
+            if self._update_latest_price(stock, date):
+                self._update_key_stats(stock, date)
                 scrape_count += 1
--- magicranker/backend/scrapers/yahoo_finance.py
+++ magicranker/backend/scrapers/yahoo_finance.py
@@ -1,314 +1,107 @@
-import re
-import time
-import logging
 from datetime import datetime
-import HTMLParser
 
-from BeautifulSoup import BeautifulSoup
-import utils
-
-
-def format_title(title):
-    title = re.sub(r'(\&nbsp\;|\,|\n|\-|\')', '', str(title).strip())
-    # Replace the space with an underscore
-    title = re.sub(r'(\ |\/)', '_', str(title.lower()))
-    return title
+import attr
+import requests
 
 
-def get_title_from_top_row(tr):
-    # Get all the <td>s in the <tr>
-    td = tr.findAll('td')
-    # If it's a bolded piece, follow these instructions
+@attr.attrs
+class PriceData(object):
+
+    """Represents price data returned from Yahoo Finance."""
+
+    stock = attr.attrib()
+    date = attr.attrib()
+    close = attr.attrib()
+    volume = attr.attrib()
+
+
+@attr.attrs
+class KeyStatsData(object):
+
+    """Represents key stats data returned from Yahoo Finance."""
+
+    stock = attr.attrib()
+    eps = attr.attrib()
+    roe = attr.attrib()
+    roa = attr.attrib()
+    bvps = attr.attrib()
+    pe = attr.attrib()
+    market_cap = attr.attrib()
+
+
+def get_key_stats(stock, req=None):
+    """Return key stats from page HTML."""
+    req = req or requests
+
+    url = (
+        'https://query1.finance.yahoo.com/v10/finance/quoteSummary/{0}.AX?'
+        'formatted=true&lang=en-US&region=AU&'
+        'modules=summaryDetail,defaultKeyStatistics,financialData,'
+        'calendarEvents&corsDomain=finance.yahoo.com').format(stock)
+    resp = req.get(url)
+    data = resp.json()
+
+    financial_data = data['quoteSummary']['result'][0]['financialData']
+
+    roe = financial_data['returnOnEquity'].get('raw')
+    roa = financial_data['returnOnAssets'].get('raw')
+
+    key_statistics = data['quoteSummary']['result'][0]['defaultKeyStatistics']
+
+    eps = key_statistics['trailingEps'].get('raw')
+    bvps = key_statistics['bookValue'].get('raw')
+
+    summary_detail = data['quoteSummary']['result'][0]['summaryDetail']
+
+    pe = summary_detail['trailingPE'].get('raw')
+    market_cap = summary_detail['marketCap'].get('raw')
+
+    return KeyStatsData(
+        stock=stock, eps=eps, roe=roe,
+        roa=roa, bvps=bvps, pe=pe, market_cap=market_cap)
+
+
+def get_current_price(stock, req=None):
+    req = req or requests
+    url = (
+        'http://download.finance.yahoo.com/d'
+        '/quotes.csv?s={0}.AX&f=sl1d1t1c1ohgv&e=.csv'.format(stock))
+    r = req.get(url)
+    page = r.text
+
+    values = None
+    if page:
+        values = page.split(',')
+
+    if not values:
+        return False
+
+    # Collect useful data from the CSV
+    date_string = values[2]
+    price = values[6]
+    volume = values[8]
+
     try:
-        title = td[0].strong.string
-        title = format_title(title)
-        return title
-    except:
-        pass
-    # If it's a bolded piece, follow these instructions
+        date = datetime.strptime(date_string, '"%m/%d/%Y"').date()
+    except ValueError:
+        return False
+
     try:
-        title = td[0].string
-        title = format_title(title)
-        return title
-    except:
-        pass
+        price = float(price)
+    except ValueError:
+        price = None
 
-
-def get_title_from_indent_row(tr):
-    # Get all the <td>s in the <tr>
-    td = tr.findAll('td')
-    # If it's a bolded piece, follow these instructions
     try:
-        title = td[1].string
-        title = format_title(title)
-        return title
-    except:
-        pass
+        volume = int(volume)
+    except ValueError:
+        volume = None
 
+    return PriceData(stock=stock, date=date, close=price, volume=volume)
 
-def get_list_from_row(tr):
-    output = []
-    td = tr.findAll('td')
-    for t in td:
-        try:
-            data = t.strong.string
-        except:
-            data = t.string
-        data = re.sub(r'(\&nbsp\;|\,|\n|\/|\')', '', str(data).strip())
-        # If data is a negative number, change format
-        if re.search(r'\(|\)', data):
-            data = re.sub(r'\(|\)', '', data)
-            data = '-' + data
-        # If Data is a number or a dash add to list
-        if re.search(r'(\d+)', data):
-            output.append(data)
-        elif re.search(r'(-)', data):
-            output.append(None)
-
-    return output
-
-
-class YahooFinance():
-    def __init__(self, stock):
-        self.stock = stock
-
-    def get_current_price(self):
-        """
-        Save the open and close price to the database
-        """
-        url = 'http://download.finance.yahoo.com/d'
-        url += '/quotes.csv?s={0}.AX&f=sl1d1t1c1ohgv&e=.csv'.format(self.stock)
-        page = utils.get_page(url)
-        values = None
-        if page:
-            values = page.split(',')
-
-        if not values:
-            return False
-
-        # Collect useful data from the CSV
-        date_string = values[2]
-        price = values[6]
-        volume = values[8]
-
-        # Convert date to datetime object
-        try:
-            self.date = datetime.strptime(date_string, '"%m/%d/%Y"').date()
-        except ValueError:
-            return False
-
-        try:
-            price = float(price)
-        except ValueError:
-            price = 0
-
-        try:
-            volume = int(volume)
-        except ValueError:
-            volume = 0
-
-        return (self.stock, self.date, price, volume)
-
-    def get_profile(self):
-        """
-        Gets the company title and description
-        @returns dict
-        """
-        h = HTMLParser.HTMLParser()
-        # Get the page data
-        url = 'http://finance.yahoo.com/q/'
-        url += 'pr?s={0}.AX+Profile'.format(self.stock)
-        page = utils.get_page(url)
-        soup = BeautifulSoup(page)
-
-        # Attempt to scrape the title
-        data = soup.find('div', 'title')
-        try:
-            name = h.unescape(data.h2.string)
-        except (AttributeError, IndexError), error:
-            logging.info(
-                "Couldn't find a title for {0}".format(self.stock))
-            logging.error(error)
-            name = ''
-        name = str(re.sub(r'(\ \(\w\w\w\.\w\w\))', '', name))
-
-        # Now, scrape the description
-        try:
-            description = soup.findAll('p')[3].string
-            if not description:
-                description = ''
-        except (AttributeError, IndexError), error:
-            logging.info(
-                "Couldn't find a description for {0}".format(self.stock))
-            logging.error(error)
-            description = ''
-
-        return (self.stock, name, description)
-
-    def get_key_stats(self):
-        """
-        Get earnings per share, roe, book value, pe and market cap
-        and return as tuple
-        """
-        url = 'http://finance.yahoo.com/q/'
-        url = url + 'ks?s={0}.AX+Key+Statistics'.format(self.stock)
-        content = utils.get_page(url)
-        soup = BeautifulSoup(''.join(str(content)))
-
-        values = soup.findAll('td', 'yfnc_tabledata1')
-
-        # Get Market Cap
-        try:
-            market_cap = values[0].span.string
-            # Remove the M or B for market cap
-            if re.search(r'M$', market_cap):
-                market_cap = re.sub(r'M', '', market_cap)
-                # Now that the M is removed,
-                # multiply it by a million to get the actual value
-                market_cap = float(market_cap)*1000000
-            elif re.search(r'B$', market_cap):
-                market_cap = re.sub(r'B', '', market_cap)
-                # Now that the B is removed,
-                # multiply it by a million to get the actual value
-                market_cap = float(market_cap)*1000000000
-            elif re.search(r'K$', market_cap):
-                market_cap = re.sub(r'K', '', market_cap)
-                # Now that the K is removed,
-                # multiply by a million to get the actual value
-                market_cap = float(market_cap)*1000
-        except:
-            market_cap = None
-
-        # Get PE
-        try:
-            pe = float(values[2].string)
-        except (ValueError, IndexError):
-            pe = None
-
-        # Get ROE
-        try:
-            roe = values[14].string
-            # Remove the percent and convert to a fraction
-            roe = re.sub(r'\%', '', roe)
-            roe = float(roe)/100
-        except (ValueError, IndexError):
-            roe = None
-
-        # Get Earnings
-        try:
-            eps = values[21].string
-            eps = float(eps)
-        except:
-            eps = None
-
-        # Get Book Value Per Share
-        try:
-            bvps = float(values[28].string)
-        except (ValueError, IndexError):
-            bvps = None
-
-        return (self.stock, eps, roe,
-                bvps, pe, market_cap)
-
-    def _get_all_assets(self, trs):
-        """
-        Get all assets
-        """
-        output = {}
-        # Current assets
-        for tr in trs[4:9]:
-            title = get_title_from_indent_row(tr)
-            output[title] = []
-            output[title] = get_list_from_row(tr)
-
-            # Total current assets
-            title = get_title_from_top_row(trs[10])
-            output[title] = []
-            output[title] = get_list_from_row(trs[10])
-
-            # Total assets fields
-            for tr in trs[11:17]:
-                title = get_title_from_top_row(tr)
-                output[title] = []
-                output[title] = get_list_from_row(trs[10])
-
-                # Total of the total assets
-                title = get_title_from_top_row(trs[19])
-                output[title] = []
-                output[title] = get_list_from_row(trs[19])
-
-        return output
-
-    def _get_all_liabilities(self, trs):
-        """
-        Get all liabilities
-        """
-        output = {}
-        # Current liabilites
-        for tr in trs[23:26]:
-                title = get_title_from_indent_row(tr)
-                output[title] = []
-                output[title] = get_list_from_row(tr)
-
-        # Get Total Current Liabilities
-        title = get_title_from_top_row(trs[27])
-        output[title] = []
-        output[title] = get_list_from_row(trs[27])
-
-        for tr in trs[28:33]:
-                title = get_title_from_top_row(tr)
-                output[title] = []
-                output[title] = get_list_from_row(tr)
-
-        title = get_title_from_top_row(trs[34])
-        output[title] = []
-        output[title] = get_list_from_row(trs[34])
-        return output
-
-    def get_bal_sheet(self):
-        """
-        Get balance sheet data
-        """
-        output = {}
-        url = 'http://finance.yahoo.com/q/'
-        url += 'bs?s={0}.AX+Balance+Sheet&annual'.format(self.stock.code)
-
-        try:
-            soup = BeautifulSoup(utils.get_page(url))
-        except:
-            print "HTTP Request failed. Skipping."
-            return False
-
-        try:
-            dates_html = soup.findAll('td', "yfnc_modtitle1")
-            date_title = dates_html[0].small.span.string
-            # Convert to lowercase and replace spaces with underscores
-            date_title = re.sub(r'(\ )', '_', str(date_title.lower()))
-        except:
-            print 'Failed to get date range. Skipping.'
-            return False
-        output[date_title] = []
-        # Get all dates
-        for date in dates_html[1:5]:
-            # Get the string from the HTML (the text is in a <b> tag)
-            # and convert to a valid time format
-            date = str(date.b.string)
-            date = time.strptime(date, "%b %d, %Y")
-            date = time.strftime("%Y-%m-%d", date)
-            output[date_title].append(date)
-
-        # Get the rest of the page data by first grabbing the main table,
-        # then the <tr>s in that
-        trs = (soup.find('table', 'yfnc_tabledata1')
-                   .find('table')
-                   .findAll('tr'))
-        output.update(self._get_all_assets(trs))
-        output.update(self._get_all_liabilities(trs))
-        return output
 
 if __name__ == '__main__':
-    for stock in ['AAZ', 'CBA']:
-        y = YahooFinance(stock)
-        y.get_profile()
-        if y.get_current_price():
-            y.get_key_stats()
+    for stock in ['ANZ', 'CBA']:
+        current_price = get_current_price(stock)
+        print "Current price", current_price
+        if current_price:
+            print "Key stats", get_key_stats(stock)
